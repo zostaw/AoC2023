de085e8ebb7af8255abdc520141d3a39f7fe67d2
Mateusz Kowalkowski <mateusz.kowalkowski@gmail.com>
2023-12-01 21:05:17 +0100

    day 1, part 1

--- day_01/src/bin/part1.rs
+++ day_01/src/bin/part1.rs
@@ -1,3 +1,29 @@
+#![feature(ascii_char)]
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = "input";
+    let file_path_example = "input.example";
+    let file_content =
+        fs::read_to_string(file_path)
+            .or(fs::read_to_string(file_path_example))
+            .unwrap_or_else(
+                |err| panic!("{err}: Expected file '{file_path}' or '{file_path_example}'")
+            );
+
+    let final_val = file_content.lines().rfold(0, |acc, line| {
+        let mut nums_only = line.matches(char::is_numeric);
+        let first = nums_only.next().unwrap_or_else(|| "0");
+        let second = match nums_only.next_back() {
+            Some(val) => val,
+            _ => first,
+        };
+        let val = vec![first, second]
+            .concat()
+            .parse::<i32>()
+            .expect("No number found in string.");
+        acc + val
+    });
+
+    println!("Sum: {}", final_val)
 }
